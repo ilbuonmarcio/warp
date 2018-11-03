1c4f8460cb3c05be66cde069864a296e0deac576
marcioz98 <marciozgaming@gmail.com>
2018-11-03 16:41:32 +0100

    Added anonymous unique name to uploaded files

--- src/app.py
+++ src/app.py
@@ -5,14 +5,22 @@ import os
 import json
 import time
 import random
+import hashlib
+import uuid
+import string
+import base64
 
 UPLOAD_FOLDER = './uploads/'
 TEMPLATE_FOLDER = './templates/'
+PASSWORD_LENGTH = 32
+MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+SECRET_KEY_LENGTH = 24
 
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
 app.debug = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = os.urandom(24)  # Set to a fixed value when putting into production environment
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+app.secret_key = os.urandom(SECRET_KEY_LENGTH)  # Set to a fixed value when putting into production environment
 app.config['SESSION_COOKIE_NAME'] = 'session_warp'
 app.config['SESSION_COOKIE_PATH'] = '/'
 
@@ -43,18 +51,30 @@ def uploadfile():
             return redirect('/')
 
         if uploaded_file:
-            filename = secure_filename(uploaded_file.filename)
+            original_filename = uploaded_file.filename
+
+            obscured_filename = base64.b64encode(
+                str.encode(
+                    str(uuid.uuid4())
+                )
+            ).decode('utf-8')
+
             uploaded_file.save(
-                os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                os.path.join(app.config['UPLOAD_FOLDER'], obscured_filename)
+            )
+            
+            password = ''.join(
+                random.choice(string.ascii_uppercase + string.digits) 
+                for _ in range(PASSWORD_LENGTH)
             )
 
             json_response = {
-                'filepath': '1234'
+                'filepath': obscured_filename
             }
 
-            session['filename'] = uploaded_file.filename
-            session['filepath'] = '1234'
-            session['password'] = '5678'
+            session['filename'] = original_filename
+            session['filepath'] = obscured_filename
+            session['password'] = password
 
             return json.dumps(json_response)
 
